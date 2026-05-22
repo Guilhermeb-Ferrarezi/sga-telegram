a453013a3346397b08b0f41a10a8bfdc95ac9adb
guilhreme <guibferrarezi@gmail.com>
2026-05-22 01:48:24 -0300

    fix: usar elixir 1.18 compativel com mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TelegramClaude.MixProject do
     [
       app: :telegram_claude,
       version: "0.1.0",
-      elixir: "~> 1.19",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
